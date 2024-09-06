000a1087e34682b9845951003b8444579aeab6e4
yun-debian <yun-debian@Debian.myguest.virtualbox.org>
2024-09-06 13:00:23 -0300

    La simulación parece bien, pero no vemos cambios en el mem dump

--- modulos/execute.sv
+++ modulos/execute.sv
@@ -1,16 +1,16 @@
-module execute(input logic AluSrc,
+module execute #(parameter N=64) (input logic AluSrc,
 					input logic [3:0] AluControl,
-					input logic [63:0] PC_E, signImm_E,
-					input logic [63:0] readData1_E, readData2_E,
-					output logic [63:0] PCBranch_E, aluResult_E, writeData_E,
+					input logic [N-1:0] PC_E, signImm_E,
+					input logic [N-1:0] readData1_E, readData2_E,
+					output logic [N-1:0] PCBranch_E, aluResult_E, writeData_E,
 					output logic zero_E);
 	
-		logic [63:0] out_mux, out_sl2, out_adder;
+		logic [N-1:0] out_mux, out_sl2, out_adder;
 		logic out_zero;
 		
-		mux2 #(.N(64)) my_mux (readData2_E, signImm_E, AluSrc, out_mux);
-		sl2 #(.N(64)) my_sl2 (signImm_E, out_sl2);
-		adder #(.N(64)) my_adder (PC_E, out_sl2, PCBranch_E);
+		mux2 #(.N(N)) my_mux (readData2_E, signImm_E, AluSrc, out_mux);
+		sl2 #(.N(N)) my_sl2 (signImm_E, out_sl2);
+		adder #(.N(N)) my_adder (PC_E, out_sl2, PCBranch_E);
 		alu my_alu (readData1_E, out_mux, AluControl, aluResult_E, out_zero);
 		
 		assign writeData_E = readData2_E;
--- modulos/fetch.sv
+++ modulos/fetch.sv
@@ -1,16 +1,16 @@
-module fetch(input logic PCSrc_F, clk, reset,
-				 input logic [63:0] PCBranch_F,
-				 output logic [63:0] imem_addr_F);
+module fetch #(parameter N=64) (input logic PCSrc_F, clk, reset,
+				 input logic [N-1:0] PCBranch_F,
+				 output logic [N-1:0] imem_addr_F);
 	
-	logic [63:0] out_pc;
-	logic [63:0] out_adder;
-	logic [63:0] out_mux;
+	logic [N-1:0] out_pc;
+	logic [N-1:0] out_adder;
+	logic [N-1:0] out_mux;
 	
-	flopr #(.N(64)) pc (clk, reset, out_mux, out_pc);
+	flopr #(.N(N)) pc (clk, reset, out_mux, out_pc);
 	
-	adder #(.N(64)) add (out_pc, 64'd4, out_adder);
+	adder #(.N(N)) add (out_pc, N'('d4), out_adder);
 	
-	mux2 #(.N(64)) my_mux (out_adder, PCBranch_F, PCSrc_F, out_mux);
+	mux2 #(.N(N)) my_mux (out_adder, PCBranch_F, PCSrc_F, out_mux);
 	
 	assign imem_addr_F = out_pc;
 	
